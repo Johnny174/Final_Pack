96e5da983df341e1158aa1a9df62a7a4605fae16
johnny <johnny.xiong1999@gmail.com>
2018-05-09 17:13:35 +0000

    READ AND REVIEW

---
+++ script6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Chapter 6 Script
+
+who -r
+
+ls /etc/init
+
+sudo systemctl list-units
+
+sudo sytemctl -p UnitPath show
+
+man systemd-idevs.service
+
+man udev
+
+ls | less
+
+#END
