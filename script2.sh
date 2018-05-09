67bcd0dcccab48a33e3f88609dbb9d86e6a03cc6
johnny <johnny.xiong1999@gmail.com>
2018-05-09 15:58:10 +0000

    READ AND REVIEW

---
+++ script2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Week 2 Script
+
+echo "Hello World!"
+echo "My name is Johnny."
+read -p "What's your name?: " x
+read -p "Where are you from?; " x
+read -p "Please enter date of birth: " x
+echo "Thank you for your time with me!"
+
+#END
